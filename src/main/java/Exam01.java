4602b2bb881c2700df41c947a85d1516da5ef229
Jeonghun-Lee <kkss4751@naver.com>
2022-03-16 11:35:23 +0900

    22/03/16 chapter 0.3 변수와 데이터 형식

---
+++ src/main/java/Exam01.java
@@ -0,0 +1,5 @@
+public class Exam01 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/Exam02.java
@@ -0,0 +1,7 @@
+public class Exam02 {
+    public static void main(String[] args) {
+        System.out.printf("100+100");
+        System.out.println();
+        System.out.printf("%d",100+100);
+    }
+}

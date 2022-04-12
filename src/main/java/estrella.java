ed29465589829eb48417c723d79e9cb36480f581
Jeonghun-Lee <kkss4751@naver.com>
2022-04-12 18:38:25 +0900

    for문복습

---
+++ src/main/java/estrella.java
@@ -0,0 +1,10 @@
+public class estrella {
+    public static void main(String[] args) {
+        int hap = 0;
+
+        for( int a = 1; a < 11; a++){
+            hap = hap + a;
+        }
+        System.out.println(hap);
+    }
+}

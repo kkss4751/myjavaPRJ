00d8cd4d455c1f48fe586d7c94c6eb6a04c21363
Jeonghun-Lee <kkss4751@naver.com>
2022-04-20 08:51:28 +0900

    0420 if / for

---
+++ src/main/java/respaso.java
@@ -0,0 +1,80 @@
+public class respaso {
+    public static void main(String[] args) {
+//        System.out.println("hello my first java");
+/*
+        int result;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("첫번째 계산할 값 입력-->");
+        int a = sc.nextInt();
+        System.out.println("두번째 계산할 값 입력-->");
+        int b = sc.nextInt();
+        System.out.println("세번째 계산할 값 입력-->");
+        int c = sc.nextInt();
+        System.out.println("네번째 계산할 값 입력-->");
+        int d = sc.nextInt();
+
+        result = a+b+c+d;
+        System.out.println(result);
+*/
+/*
+        int result;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("첫번째 계산할 값을 입력하세요");
+        int a = sc.nextInt();
+        System.out.println("<1>덧셈,<2>뺄셈,<3>곱셈,<4>나눗셈");
+        int b = sc.nextInt();
+        System.out.println("두번째 계산할 값을 입력하세요");
+        int c = sc.nextInt();
+
+        if ( b == 1) {
+            result = a + c;
+            System.out.println(a + "+" + b + "=" + result);
+        }else if ( b == 2) {
+            result = a - c;
+            System.out.println(a + "-" + b + "=" + result);
+        }else if ( b == 3) {
+            result = a * c;
+            System.out.println(a + "x" + b + "=" + result);
+        }else {
+            result = a / c;
+            System.out.println(a + "/" + b + "=" + result);
+        }*/
+/*
+        Scanner sc= new Scanner(System.in);
+        System.out.printf("숫자를 입력하세요");
+        int a = sc.nextInt();
+
+        if(((a % 3 == 0) && (a % 5 == 0))) {
+            System.out.printf("3과 5의 배수입니다\n");
+        }else if ( a % 3 == 0) {
+            System.out.printf("3의 배수입니다");
+        }else if ( a % 5 == 0) {
+            System.out.printf("5의 배수입니다");
+        }else System.out.printf("잘못입력하셨습니다.");
+*/
+        /*Scanner sc = new Scanner(System.in);
+        System.out.printf("출력할 단 입력");
+        int dan = sc.nextInt();
+
+        int i =1;
+        while ( i <10) {
+            System.out.printf("%d x %d = %d\n", dan , i, dan*i);
+            i++;*/
+        /*
+        for( int i = 1; i < 10; i++) {
+            System.out.printf("%d x %d = %d\n",dan,i,dan*i);
+        }*/
+
+        /*Scanner s = new Scanner(System.in);
+        System.out.printf("출력할 값을 입력하시오");
+        int dan = s.nextInt();
+        System.out.printf("어디까지 곱할것인가");
+        int f = s.nextInt();
+
+        for( int i = dan; i <= dan; i++) {
+            for ( int n = 1; n <= f; n++) {
+                System.out.printf("%d x %d = %d\n",dan,n,dan*n);
+            }
+        }*/
+    }
+}

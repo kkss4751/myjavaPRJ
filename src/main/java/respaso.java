72fd46b918e3018fd4d52c3415cf85fe56ebc64d
Jeonghun-Lee <kkss4751@naver.com>
2022-04-22 13:52:27 +0900

    0422/ c언어 java바꿈

--- src/main/java/respaso.java
+++ src/main/java/respaso.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class respaso {
     public static void main(String[] args) {
 //        System.out.println("hello my first java");
@@ -118,7 +116,7 @@ public class respaso {
             System.out.println();
         }*/
 
-                Scanner input = new Scanner(System.in);
+/*                Scanner input = new Scanner(System.in);
                 int x;
                 int y;
                 int sum;
@@ -132,5 +130,37 @@ public class respaso {
                 sum = x + y;
 
                 System.out.println(sum);
+    */
+        int month = 8;
+        String monthString = "";
+        switch (month) {
+            case 1:  monthString = "January";
+                break;
+            case 2:  monthString = "February";
+                break;
+            case 3:  monthString = "March";
+                break;
+            case 4:  monthString = "April";
+                break;
+            case 5:  monthString = "May";
+                break;
+            case 6:  monthString = "June";
+                break;
+            case 7:  monthString = "July";
+                break;
+            case 8:  monthString = "August";
+                break;
+            case 9:  monthString = "September";
+                break;
+            case 10: monthString = "October";
+                break;
+            case 11: monthString = "November";
+                break;
+            case 12: monthString = "December";
+                break;
+            default: monthString = "Invalid month";
+                break;
+        }
+        System.out.println(monthString);
     }
 }

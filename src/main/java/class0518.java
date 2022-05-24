0d475c529cb4ea97b5a2a6f6afd3e07babad8bdd
Jeonghun-Lee <kkss4751@naver.com>
2022-05-24 21:14:15 +0900

    0513 백준 연습 0517 자바 수업내용 복습 0518 class 1~3 java 문자열 0519 수업내용 복습 0523 stack class 기초문제 0524 최대값 최소값

--- src/main/java/class0518.java
+++ src/main/java/class0518.java
@@ -1,10 +1,23 @@
 import java.util.Scanner;
-import java.util.Stack;
 
 public class class0518 {
     public static void main(String[] args) {
 
-                Scanner sc = new Scanner(System.in);
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+        int max = -1000000;
+        int min = 1000000;
+
+        for (int i = 0; i < N; i++) {
+            int x = sc.nextInt();
+            if(x >= max) max = x;
+            if(x <= min) min = x;
+        }
+        System.out.println(min + " " + max);
+
+               /* Scanner sc = new Scanner(System.in);
 
                 int n = sc.nextInt();
 
@@ -15,7 +28,7 @@ public class class0518 {
                 }
                 for (int i = 0; i <n ; i++) {
                     System.out.printf("%d ",stack.pop());
-                }
+                }*/
 
 
 

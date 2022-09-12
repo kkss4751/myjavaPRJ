aa2db10817f18a626fc511220f62b4bed0f0c6f1
Jeonghun-Lee <kkss4751@naver.com>
2022-09-13 07:44:25 +0900

    CodeUp 단순 for문

---
+++ src/main/java/codeUp/CodeUp1251.java
@@ -0,0 +1,10 @@
+package codeUp;
+
+/** 1부터 100까지 출력하기 **/
+public class CodeUp1251 {
+    public static void main(String[] args) {
+        for (int j = 1; j<= 100; j++ ){
+            System.out.printf("%d ",j);
+        }
+    }
+}
---
+++ src/main/java/codeUp/CodeUp1252.java
@@ -0,0 +1,16 @@
+package codeUp;
+
+import java.util.Scanner;
+
+public class CodeUp1252 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+
+        for (int j = 1; j <= i; j++){
+            System.out.printf("%d ",j);
+        }
+
+    }
+}
